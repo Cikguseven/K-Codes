9c0e4eb878f8b5d0ce60d7910c789df571debd84
Kieron Seven Lee <kieronsevenlee@u.nus.edu>
2024-03-18 23:37:54 +0800

    reusable role js

---
+++ assets/js/common.js
@@ -0,0 +1,6 @@
+(function(){
+    var role = "Year 2 NUS Computer Science";
+    document.getElementById("role").innerHTML = role;
+    var updated = "Last Updated: March 2024";
+    document.getElementById("updated").innerHTML = updated;
+})();
--- assets/js/lastupdated.js
+++
@@ -1,4 +0,0 @@
-(function(){
-    var text = "Last Updated: March 2024";
-    document.getElementById("updated").innerHTML = text;
-})();
